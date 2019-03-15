0abf0c7ddb92eb25d9d3ffb63433fed941c15b44
Eddie <stoke081@umn.edu>
2019-03-14 20:23:00 -0500

    update

---
+++ PyBank/main.py
@@ -0,0 +1,27 @@
+#import csv file
+import os
+import csv
+import pandas as pd
+
+homework3_csv = os.path.expanduser('~/Desktop/homework3/python-challenge/PyBank/budget_data.csv')
+file = "budget_data.csv"
+budget_pd = pd.read_csv(file)
+
+Months = len(budget_pd)
+PLtotal = budget_pd["Profit/Losses"].sum()
+MonthlyDiff = budget_pd["Profit/Losses"].diff()
+MonthlyChange = MonthlyDiff.dropna(how='any')
+AvgChange = MonthlyChange.mean()
+AvgChangeMax = MonthlyChange.max()
+AvgChangeMin = MonthlyChange.min()
+
+print(f'Total Months: {Months}')
+print(f'Net Profit/Losses: {PLtotal}')
+print(f'Average Monthly Change: {AvgChange}')
+print(f'Greatest Increase: {AvgChangeMax}')
+print(f'Greatest Decrease: {AvgChangeMin}')
+
+
+    
+    
+    
